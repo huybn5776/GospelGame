ed562681ce6d47458caadd5714add9d94a542ddf
huybn5776 <aseeme555076@gmail.com>
2019-10-18 21:12:35 +0800

    Refactor allGameItems to single file.

---
+++ src/app/constants/all-game-items.ts
@@ -0,0 +1,9 @@
+import { GameItemInfo } from '../entities/game-item-info';
+
+export const allGameItems: GameItemInfo[] = [
+  {id: 'mushroom', name: '蘑菇', src: 'assets/img/item-mushroom.png'},
+  {id: 'golden-mushroom', name: '衝剌蘑菇', src: 'assets/img/item-golden-mushroom.png'},
+  {id: 'fake-item-box', name: '假道具', src: 'assets/img/item-fake-item-box.png'},
+  {id: 'spiny-shell', name: '飛龜', src: 'assets/img/item-spiny-shell.png'},
+  {id: 'star', name: '無敵星', src: 'assets/img/item-star.png'},
+];
--- src/app/modules/container/input-score/game-item-select/game-item-select.component.ts
+++ src/app/modules/container/input-score/game-item-select/game-item-select.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, forwardRef, Input, Output } from '@angular/cor
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 import { GameItemInfo } from '../../../../entities/game-item-info';
+import { allGameItems } from '../../../../constants/all-game-items';
 
 @Component({
   selector: 'app-game-item-select',
@@ -17,13 +18,7 @@ export class GameItemSelectComponent implements ControlValueAccessor {
 
   onChange: (value) => void;
 
-  gameItems: GameItemInfo[] = [
-    {id: 'mushroom', name: '蘑菇', src: 'assets/img/item-mushroom.png'},
-    {id: 'golden-mushroom', name: '衝剌蘑菇', src: 'assets/img/item-golden-mushroom.png'},
-    {id: 'fake-item-box', name: '假道具', src: 'assets/img/item-fake-item-box.png'},
-    {id: 'spiny-shell', name: '飛龜', src: 'assets/img/item-spiny-shell.png'},
-    {id: 'star', name: '無敵星', src: 'assets/img/item-star.png'},
-  ];
+  gameItems: GameItemInfo[] = allGameItems;
 
   registerOnChange(fn: (value) => void) {
     this.onChange = fn;
